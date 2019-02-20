105be7f82464c535d1ee99c01fb30293024eaa78
Dan Stocker <dan@kwaia.com>
2019-02-20 19:58:40 +0000

    Refactors gravel-routing to use createNode()

--- modules/routing/src/nodes/Router.ts
+++ modules/routing/src/nodes/Router.ts
@@ -1,33 +1,30 @@
-import {createOutPorts, createOutputs, InPorts, Node} from "river-core";
+import {createNode, Node} from "river-core";
 
-export type Inputs = {
+export type In = {
   d_path: string;
 };
 
-export type Outputs = {
+export type Out = {
   [path: string]: Array<any>;
 };
 
-export type Router = Node<Inputs, Outputs>;
+export type Router = Node<In, Out>;
 
 const slice = Array.prototype.slice;
 
 export function createRouter<R extends Array<RegExp>>(routes: R): Router {
   const paths = routes.map(String);
-  const o = createOutPorts(paths);
-  const outputs = createOutputs(o);
-
-  const i: InPorts<Inputs> = {
-    d_path: (value, tag) => {
-      for (const [idx, route] of routes.entries()) {
-        const hits = route.exec(value);
-        if (hits) {
-          outputs[paths[idx]](slice.call(hits, 1), tag);
-          break;
+  return createNode<In, Out>(paths, (outputs) => {
+    return {
+      d_path: (value, tag) => {
+        for (const [idx, route] of routes.entries()) {
+          const hits = route.exec(value);
+          if (hits) {
+            outputs[paths[idx]](slice.call(hits, 1), tag);
+            break;
+          }
         }
       }
-    }
-  };
-
-  return {i, o};
+    };
+  });
 }
