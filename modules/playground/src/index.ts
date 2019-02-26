fcd4ebfd2fb8b8e48208d5d3eb7ccfca6744f4fe
danstocker <dan@kwaia.com>
2019-02-26 19:39:50 +0000

    Separates element creation from update in playground

--- modules/playground/src/index.ts
+++ modules/playground/src/index.ts
@@ -8,9 +8,14 @@ connect(locationHash.o.d_val, () => {
   applyView({
     del: {},
     set: {
-      "body.childNodes.4.attributes.bar": "baz",
-      "body.childNodes.4.classList.foo": true,
       "body.childNodes.4.tagName": "section"
+    }
+  });
+  applyView({
+    del: {},
+    set: {
+      "body.childNodes.4.attributes.bar": "baz",
+      "body.childNodes.4.classList.foo": true
     }
   });
 });
