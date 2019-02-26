f551230f092f873d8d2331e4fdbcce4a0c197693
Dan Stocker <dan@kwaia.com>
2019-02-26 06:35:19 +0000

    Changes return type of setDomPath()

--- modules/dom/src/utils/utils.ts
+++ modules/dom/src/utils/utils.ts
@@ -1,38 +1,36 @@
-export const pathValueCache = {};
-
-export function setDomPath(path: string, value: any): void {
+export function setDomPath(path: string, value: any): boolean {
   const keys = path.split(".");
   let tmp: any = document;
   let parent: Node = document;
   do {
     const key = keys.shift();
-    if (tmp instanceof Comment) {
-      if (key === "tagName") {
-        const element = document.createElement(key);
-        tmp.parentNode.replaceChild(element, tmp);
-      }
+    if (tmp instanceof Comment && key === "tagName") {
+      const element = document.createElement(key);
+      tmp.parentNode.replaceChild(element, tmp);
     } else if (tmp instanceof Node) {
       parent = tmp;
       tmp = tmp[key];
     } else if (tmp instanceof NodeList) {
       if (tmp[key] === undefined) {
         addPlaceholders(parent, parseInt(key, 10));
-        pathValueCache[path] = value;
+        return false;
       } else {
         tmp = tmp[key];
       }
-    } else if (tmp instanceof NamedNodeMap) {
-      if (!tmp.getNamedItem(key)) {
-        const attribute = document.createAttribute(key);
-        attribute.value = value;
-        tmp.setNamedItem(attribute);
-      }
+    } else if (tmp instanceof NamedNodeMap && !tmp.getNamedItem(key)) {
+      const attribute = document.createAttribute(key);
+      attribute.value = value;
+      tmp.setNamedItem(attribute);
     } else if (tmp instanceof DOMTokenList) {
       tmp.add(key, key);
     } else {
       tmp = tmp[key];
     }
+    // tslint:disable
+    console.log(">", key, tmp);
   } while (tmp !== undefined && keys.length);
+
+  return tmp !== undefined;
 }
 
 export function addPlaceholders(parent: Node, index: number): void {
