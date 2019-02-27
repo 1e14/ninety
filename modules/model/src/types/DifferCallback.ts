a8dcb4930809f1cce0a1456dc0e4df351b85843d
Dan Stocker <dan@kwaia.com>
2019-02-27 20:50:22 +0000

    Removes unused types from gravel-model

--- modules/model/src/types/DifferCallback.ts
+++
@@ -1,5 +0,0 @@
-import {Diff} from "gravel-types";
-import {Any} from "river-core";
-
-export type DifferCallback<T extends Any> =
-  (before: Partial<T>, after: Partial<T>) => Diff<T>;
--- modules/model/src/types/MergerCallback.ts
+++
@@ -1,5 +0,0 @@
-import {Diff} from "gravel-types";
-import {Any} from "river-core";
-
-export type MergerCallback<T extends Any> =
-  (current: Partial<T>, diff: Diff<T>) => void;
