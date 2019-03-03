597f22418a990bc13236d973c4ad8ef314662171
Dan Stocker <dan@kwaia.com>
2019-03-03 12:11:46 +0000

    Refactors DiffBuffer to reset instead of open / close

--- modules/core/src/nodes/DiffBuffer.spec.ts
+++ modules/core/src/nodes/DiffBuffer.spec.ts
@@ -2,57 +2,35 @@ import {connect} from "river-core";
 import {createDiffBuffer, DiffBuffer} from "./DiffBuffer";
 
 describe("createDiffBuffer()", () => {
-  describe("on input (d_diff)", () => {
+  describe("on input (ev_res)", function () {
     let node: DiffBuffer<{ foo: number, bar: boolean }>;
 
-    describe("when open", () => {
+    beforeEach(() => {
+      node = createDiffBuffer();
+    });
+
+    describe("after multiple diffs", () => {
       beforeEach(() => {
-        node = createDiffBuffer(true);
+        node.i.d_diff({
+          set: {foo: 5}
+        });
+        node.i.d_diff({
+          del: {foo: 5},
+          set: {bar: true}
+        });
+        node.i.d_diff({
+          set: {foo: 3, bar: false}
+        });
       });
 
-      it("should forward 'd_diff'", () => {
+      it("should emit aggregated diff on 'd_diff'", () => {
         const spy = jasmine.createSpy();
         connect(node.o.d_diff, spy);
-        const diff = {
-          set: {foo: 5}
-        };
-        node.i.d_diff(diff, "1");
-        expect(spy).toHaveBeenCalledWith(diff, "1");
-      });
-    });
-  });
-
-  describe("on input (st_open)", function () {
-    let node: DiffBuffer<{ foo: number, bar: boolean }>;
-
-    describe("on opening", () => {
-      beforeEach(() => {
-        node = createDiffBuffer(false);
-      });
-
-      describe("after multiple diffs", () => {
-        beforeEach(() => {
-          node.i.d_diff({
-            set: {foo: 5}
-          });
-          node.i.d_diff({
-            del: {foo: 5},
-            set: {bar: true}
-          });
-          node.i.d_diff({
-            set: {foo: 3, bar: false}
-          });
-        });
-
-        it("should emit aggregated diff on 'd_diff'", () => {
-          const spy = jasmine.createSpy();
-          connect(node.o.d_diff, spy);
-          node.i.st_open(true, "1");
-          expect(spy).toHaveBeenCalledWith({
-            del: {},
-            set: {foo: 3, bar: false}
-          }, "1");
-        });
+        node.i.ev_res(null, "1");
+        expect(spy).toHaveBeenCalledWith({
+          del: {},
+          set: {foo: 3, bar: false}
+        }, "1");
       });
     });
   });
--- modules/core/src/nodes/DiffBuffer.ts
+++ modules/core/src/nodes/DiffBuffer.ts
@@ -3,7 +3,7 @@ import {Diff} from "../types";
 
 export type In<T> = {
   d_diff: Diff<T>;
-  st_open: boolean;
+  ev_res: any;
 };
 
 export type Out<T> = {
@@ -12,7 +12,7 @@ export type Out<T> = {
 
 export type DiffBuffer<T> = Node<In<T>, Out<T>>;
 
-export function createDiffBuffer<T>(open?: boolean): DiffBuffer<T> {
+export function createDiffBuffer<T>(): DiffBuffer<T> {
   return createNode<In<T>, Out<T>>
   (["d_diff"], (outputs) => {
     let buffer: Diff<T> = {
@@ -22,42 +22,35 @@ export function createDiffBuffer<T>(open?: boolean): DiffBuffer<T> {
 
     return {
       d_diff: (value, tag) => {
-        if (open) {
-          outputs.d_diff(value, tag);
-        } else {
-          const bufferSet = buffer.set;
-          const bufferDel = buffer.del;
+        const bufferSet = buffer.set;
+        const bufferDel = buffer.del;
 
-          // transferring deletes
-          for (const path in value.del) {
-            if (path in bufferSet) {
-              delete bufferSet[path];
-            } else {
-              bufferDel[path] = null;
-            }
+        // transferring deletes
+        for (const path in value.del) {
+          if (path in bufferSet) {
+            delete bufferSet[path];
+          } else {
+            bufferDel[path] = null;
           }
+        }
 
-          // transferring sets
-          const valueSet = value.set;
-          for (const path in value.set) {
-            if (path in bufferDel) {
-              delete bufferDel[path];
-            } else {
-              bufferSet[path] = valueSet[path];
-            }
+        // transferring sets
+        const valueSet = value.set;
+        for (const path in value.set) {
+          if (path in bufferDel) {
+            delete bufferDel[path];
+          } else {
+            bufferSet[path] = valueSet[path];
           }
         }
       },
 
-      st_open: (value, tag) => {
-        if (value && !open) {
-          outputs.d_diff(buffer, tag);
-          buffer = {
-            del: {},
-            set: {}
-          };
-        }
-        open = value;
+      ev_res: (value, tag) => {
+        outputs.d_diff(buffer, tag);
+        buffer = {
+          del: {},
+          set: {}
+        };
       }
     };
   });
