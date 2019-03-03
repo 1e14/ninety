d9bce6d18b311287e1ae276b707b4244a184703d
Dan Stocker <dan@kwaia.com>
2019-03-03 12:25:26 +0000

    Refactors demo bootstrap automatically

--- modules/demo/src/index.ts
+++ modules/demo/src/index.ts
@@ -1,30 +1,34 @@
+import {createDiffBuffer} from "gravel-core";
 import {createDomDiffApplier} from "gravel-view-dom";
 import {createLocationHash} from "river-browser";
 import {connect} from "river-core";
 
 const locationHash = createLocationHash();
+const diffBuffer = createDiffBuffer();
 const domDiffApplier = createDomDiffApplier();
 
-connect(locationHash.o.d_val, () => {
-  domDiffApplier.i.d_diff({
-    set: {
-      "body.childNodes.1:section": null,
-      "body.childNodes.4:section.attributes.bar": "baz",
-      "body.childNodes.4:section.childNodes.0:span.innerText": "Hello World!",
-      "body.childNodes.4:section.childNodes.0:span.style.color": "red",
-      "body.childNodes.4:section.classList.foo": true,
-      "body.childNodes.4:section.style.backgroundColor": "green",
-      "body.childNodes.4:section.style.height": "100px"
-    }
-  });
-  domDiffApplier.i.d_diff({
-    del: {
-      "body.childNodes.1:section": null,
-      "body.childNodes.4:section.classList.foo": null
-    },
-    set: {
-      "body.childNodes.2:div.attributes.id": "quux",
-      "body.childNodes.4:section.attributes.bar": "BAZ"
-    }
-  });
+connect(locationHash.o.d_val, diffBuffer.i.ev_res);
+connect(diffBuffer.o.d_diff, domDiffApplier.i.d_diff);
+
+diffBuffer.i.d_diff({
+  set: {
+    "body.childNodes.1:section": null,
+    "body.childNodes.4:section.attributes.bar": "baz",
+    "body.childNodes.4:section.childNodes.0:span.innerText": "Hello World!",
+    "body.childNodes.4:section.childNodes.0:span.style.color": "red",
+    "body.childNodes.4:section.classList.foo": true,
+    "body.childNodes.4:section.style.backgroundColor": "green",
+    "body.childNodes.4:section.style.height": "100px"
+  }
+});
+
+diffBuffer.i.d_diff({
+  del: {
+    "body.childNodes.1:section": null,
+    "body.childNodes.4:section.classList.foo": null
+  },
+  set: {
+    "body.childNodes.2:div.attributes.id": "quux",
+    "body.childNodes.4:section.attributes.bar": "BAZ"
+  }
 });
