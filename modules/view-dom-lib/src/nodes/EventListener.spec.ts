9a7afe5b5e4ea58286d79a3ef1c002be1e042114
Dan Stocker <dan@kwaia.com>
2019-03-03 20:55:03 +0000

    Fixes EventListener, adds specs

---
+++ modules/view-dom-lib/src/nodes/EventListener.spec.ts
@@ -0,0 +1,58 @@
+import {connect} from "river-core";
+import {createEventListener, EventListener} from "./EventListener";
+
+const window = <any>global;
+
+beforeEach(() => {
+  window.Event = function () {//
+  };
+  window.Event.prototype = {
+    stopImmediatePropagation: () => null
+  };
+});
+
+afterEach(() => {
+  delete window.Event;
+});
+
+describe("createEventListener()", () => {
+  describe("on input (d_event)", () => {
+    let node: EventListener<Event>;
+
+    beforeEach(() => {
+      node = createEventListener("onclick");
+    });
+
+    it("should stop event propagation", () => {
+      const event = new window.Event("foo");
+      spyOn(event, "stopImmediatePropagation");
+      node.i.d_event(event, "1");
+      expect(event.stopImmediatePropagation).toHaveBeenCalled();
+    });
+
+    it("should emit on 'd_event'", () => {
+      const spy = jasmine.createSpy();
+      connect(node.o.d_event, spy);
+      const event = new window.Event("foo");
+      node.i.d_event(event, "1");
+      expect(spy).toHaveBeenCalledWith(event, "1");
+    });
+  });
+
+  describe("on input (ev_smp)", () => {
+    let node: EventListener<Event>;
+
+    beforeEach(() => {
+      node = createEventListener("onclick");
+    });
+
+    it("should emit on 'd_diff'", () => {
+      const spy = jasmine.createSpy();
+      connect(node.o.d_diff, spy);
+      node.i.ev_smp(null, "1");
+      expect(spy).toHaveBeenCalledWith({
+        set: {onclick: node.i.d_event}
+      }, "1");
+    });
+  });
+});
--- modules/view-dom-lib/src/nodes/EventListener.ts
+++ modules/view-dom-lib/src/nodes/EventListener.ts
@@ -2,26 +2,32 @@ import {Diff} from "gravel-core";
 import {createNode, Node} from "river-core";
 import {DomEventType} from "../types";
 
-export type In = {
-  d_event: Event;
+export type DomEventHandler = (event: Event) => void;
+
+export type DomEventHandlers = {
+  [key in DomEventType]: DomEventHandler
+};
+
+export type EventListenerDiff = Diff<DomEventHandlers>;
+
+export type In<T extends Event> = {
+  d_event: T;
   ev_smp: any;
 };
 
-export type Out = {
-  d_event: Event;
-  d_diff: Diff<{
-    [key in DomEventType]: (event: Event) => void;
-  }>
+export type Out<T extends Event> = {
+  d_event: T;
+  d_diff: EventListenerDiff
 };
 
-export type EventListener = Node<In, Out>;
+export type EventListener<T extends Event> = Node<In<T>, Out<T>>;
 
-export function createEventListener(type: DomEventType): EventListener {
-  return createNode<In, Out>(["d_event"], (outputs) => {
+export function createEventListener<T extends Event>(type: DomEventType): EventListener<T> {
+  return createNode<In<T>, Out<T>>(["d_diff", "d_event"], (outputs) => {
     const i = {
-      d_event: (event) => {
+      d_event: (event, tag) => {
         event.stopImmediatePropagation();
-        outputs.d_event(event);
+        outputs.d_event(event, tag);
       },
 
       ev_smp: (value, tag) => {
