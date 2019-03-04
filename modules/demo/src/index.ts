5f3fbd4033cd80d2f26e1c75bf292bd5c9c4c5bd
danstocker <dan@kwaia.com>
2019-03-04 17:51:07 +0000

    Sets up routes in demo

--- modules/demo/src/index.ts
+++ modules/demo/src/index.ts
@@ -1,6 +1,7 @@
 // tslint:disable:no-console
 
 import {createDiffBuffer} from "gravel-core";
+import {createRouter} from "gravel-routing";
 import {createDomDiffApplier} from "gravel-view-dom";
 import {createPageView} from "gravel-view-dom-lib";
 import {createLocationHash} from "river-browser";
@@ -8,23 +9,38 @@ import {connect} from "river-core";
 import {createMapper, createNoop} from "river-stdlib";
 import {createCustomTextView} from "./nodes/CustomTextView";
 
+// setting up hash-based routing
 const locationHash = createLocationHash();
+const hash2Path = createMapper<string, string>((hash) => hash.substr(1));
+connect(locationHash.o.d_val, hash2Path.i.d_val);
+
+// setting up rendering engine
+// flushes diff buffer to renderer every 10ms
 const ticker = createNoop();
 const diffBuffer = createDiffBuffer();
 const domDiffApplier = createDomDiffApplier();
-
-connect(locationHash.o.d_val, diffBuffer.i.ev_res);
-connect(diffBuffer.o.d_diff, domDiffApplier.i.d_diff);
-// connect(diffBuffer.o.d_diff, console.log);
-setInterval(ticker.i.d_val, 10);
-connect(ticker.o.d_val, diffBuffer.i.ev_res);
-
 const pageView = createPageView();
-const textView = createCustomTextView("childNodes.1:span");
-const textSource = createMapper(() => "Hello World!");
-
-connect(locationHash.o.d_val, textSource.i.d_val);
-connect(textSource.o.d_val, textView.i.d_content);
-connect(textView.o.d_diff, pageView.i.d_diff);
-connect(textView.o.ev_click, console.log);
+setInterval(ticker.i.d_val, 10);
+connect(diffBuffer.o.d_diff, domDiffApplier.i.d_diff);
+connect(ticker.o.d_val, diffBuffer.i.ev_res);
 connect(pageView.o.d_diff, diffBuffer.i.d_diff);
+
+// setting up routing table
+const router = createRouter([
+  /^render$/,
+  /^views$/
+]);
+connect(hash2Path.o.d_val, router.i.d_path);
+
+// setting up routing "table"
+connect(router.o["/^views$/"], () => {
+  const textView = createCustomTextView("childNodes.1:span");
+  const textSource = createMapper(() => "Hello World!");
+
+  connect(textSource.o.d_val, textView.i.d_content);
+  connect(textView.o.d_diff, pageView.i.d_diff);
+  connect(textView.o.ev_click, console.log);
+
+  // nudge
+  textSource.i.d_val(null);
+});
