e86b84ea292d3054ddba1469befa606e36179057
Dan Stocker <dan@kwaia.com>
2019-03-06 07:30:19 +0000

    Removes styleSource from CustomTextView

--- modules/demo/src/nodes/CustomTextView.ts
+++ modules/demo/src/nodes/CustomTextView.ts
@@ -1,12 +1,11 @@
-import {createDiffPrefixer, Diff} from "gravel-core";
-import {ViewIn, ViewOut} from "gravel-view-dom";
+import {createView, ViewIn, ViewOut} from "gravel-view-dom";
 import {
   createEventListener,
   createTextView,
   TextVmProps
 } from "gravel-view-dom-lib";
 import {connect, InPorts, Node, OutPorts} from "river-core";
-import {createMapper, createNoop} from "river-stdlib";
+import {createNoop} from "river-stdlib";
 
 export type In = ViewIn<TextVmProps>;
 
@@ -19,20 +18,18 @@ export type CustomTextView = Node<In, Out>;
 export function createCustomTextView(prefix: string = ""): CustomTextView {
   const input = createNoop();
   const textView = createTextView(prefix);
-  const styleSource = createMapper<any, Diff<any>>(() => ({
+  const styleView = createView(prefix, () => ({
     set: {
       "style.color": "red"
     }
   }));
-  const styleView = createDiffPrefixer(prefix);
   const clickListener = createEventListener(prefix, "onclick");
   const output = createNoop();
 
   connect(input.o.d_val, textView.i.vm_diff);
   connect(input.o.d_val, clickListener.i.ev_smp);
-  connect(input.o.d_val, styleSource.i.d_val);
-  connect(styleSource.o.d_val, styleView.i.d_diff);
-  connect(styleView.o.d_diff, output.i.d_val);
+  connect(input.o.d_val, styleView.i.vm_diff);
+  connect(styleView.o.v_diff, output.i.d_val);
   connect(clickListener.o.d_diff, output.i.d_val);
   connect(textView.o.v_diff, output.i.d_val);
 
