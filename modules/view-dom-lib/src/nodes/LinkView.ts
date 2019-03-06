244d1472c8eb279a137eb00eab3b84611f165e59
Dan Stocker <dan@kwaia.com>
2019-03-06 07:13:00 +0000

    Adds LinkView

--- modules/view-dom-lib/src/nodes/LinkView.ts
+++ modules/view-dom-lib/src/nodes/LinkView.ts
@@ -3,23 +3,30 @@ import {createView, ViewIn, ViewOut} from "gravel-view-dom";
 import {Node} from "river-core";
 
 export type VmProps = {
-  content: string
+  url: string;
+  content: string;
 };
 
 export type In = ViewIn<VmProps>;
 
 export type Out = ViewOut;
 
-export type TextView = Node<In, Out>;
+export type LinkView = Node<In, Out>;
 
-export function createTextView(
+export function createLinkView(
   path: string = "",
   content: Diff<any> = {}
-): TextView {
+): LinkView {
   return createView<VmProps>(path, content, (vm) => {
     const set = vm.set;
+    const del = vm.del;
     return {
+      del: {
+        href: del && del.url,
+        innerText: del && del.content
+      },
       set: {
+        href: set && set.url,
         innerText: set && set.content
       }
     };
--- modules/view-dom-lib/src/nodes/index.ts
+++ modules/view-dom-lib/src/nodes/index.ts
@@ -1,3 +1,4 @@
 export {EventListener, createEventListener} from "./EventListener";
+export {LinkView, createLinkView, VmProps as LinkVmProps} from "./LinkView";
 export {PageView, createPageView} from "./PageView";
 export {TextView, createTextView, VmProps as TextVmProps} from "./TextView";
