931ad9e7418c3bafaca5c77e1246ce24912e992f
Dan Stocker <dan@kwaia.com>
2019-03-07 20:54:16 +0000

    Removes set & del checks in prefixDiffPaths()

--- modules/core/src/utils/utils.ts
+++ modules/core/src/utils/utils.ts
@@ -8,22 +8,17 @@ import {Diff} from "../types";
 export function prefixDiffPaths<T>(
   diff: Diff<T>, prefix: string
 ): Diff<{ [key: string]: T[keyof T] }> {
-  const result = <Diff<T>>{};
   const diffSet = diff.set;
-  if (diffSet) {
-    const set = result.set = {};
-    for (const key in diff.set) {
-      set[prefix + "." + key] = diff.set[key];
-    }
-  }
   const diffDel = diff.del;
-  if (diffDel) {
-    const del = result.del = {};
-    for (const key in diff.del) {
-      del[prefix + "." + key] = diff.del[key];
-    }
+  const set = {};
+  const del = {};
+  for (const key in diffSet) {
+    set[prefix + "." + key] = diffSet[key];
   }
-  return result;
+  for (const key in diffDel) {
+    del[prefix + "." + key] = diffDel[key];
+  }
+  return {set, del};
 }
 
 /**
