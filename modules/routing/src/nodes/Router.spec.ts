c9befec8b6fe1dbc5efabc4c6d9c84499d38ac5e
Dan Stocker <dan@kwaia.com>
2019-03-08 14:13:18 +0000

    Adds filtering to Router

--- modules/routing/src/nodes/Router.spec.ts
+++ modules/routing/src/nodes/Router.spec.ts
@@ -13,11 +13,51 @@ describe("createRouter()", () => {
     });
 
     describe("when path matches route", () => {
-      it("should emit on corresponding port", () => {
+      it("should emit params on corresponding port", () => {
         const spy = jasmine.createSpy();
         connect(node.o["r_/^user\\/(\\d+)$/"], spy);
-        node.i.d_path("user/100", "1");
+        node.i.d_route("user/100", "1");
         expect(spy).toHaveBeenCalledWith(["100"], "1");
+      });
+
+      it("should emit route on 'd_template'", () => {
+        const spy = jasmine.createSpy();
+        connect(node.o.d_template, spy);
+        node.i.d_route("user/100", "1");
+        expect(spy).toHaveBeenCalledWith(/^user\/(\d+)$/, "1");
+      });
+
+      describe("but route did not change", function () {
+        beforeEach(() => {
+          node.i.d_route("user/100", "1");
+        });
+
+        it("should not emit on 'd_template'", () => {
+          const spy = jasmine.createSpy();
+          connect(node.o.d_template, spy);
+          node.i.d_route("user/200", "2");
+          expect(spy).not.toHaveBeenCalled();
+        });
+      });
+
+      describe("but path did not change", function () {
+        beforeEach(() => {
+          node.i.d_route("user/100", "1");
+        });
+
+        it("should not emit on route ports", () => {
+          const spy = jasmine.createSpy();
+          connect(node.o["r_/^user\\/(\\d+)$/"], spy);
+          node.i.d_route("user/100", "2");
+          expect(spy).not.toHaveBeenCalled();
+        });
+
+        it("should not emit on 'd_template'", () => {
+          const spy = jasmine.createSpy();
+          connect(node.o.d_template, spy);
+          node.i.d_route("user/100", "2");
+          expect(spy).not.toHaveBeenCalled();
+        });
       });
     });
 
@@ -25,7 +65,7 @@ describe("createRouter()", () => {
       it("should not emit on corresponding port", () => {
         const spy = jasmine.createSpy();
         connect(node.o["r_/^user\\/(\\d+)$/"], spy);
-        node.i.d_path("foo/100", "1");
+        node.i.d_route("foo/100", "1");
         expect(spy).not.toHaveBeenCalled();
       });
     });
--- modules/routing/src/nodes/Router.ts
+++ modules/routing/src/nodes/Router.ts
@@ -1,10 +1,13 @@
 import {createNode, Node} from "river-core";
+import {eqArray} from "../callbacks";
 
 export type In = {
-  d_path: string;
+  d_route: string;
 };
 
 export type Out = {
+  d_template: RegExp
+} & {
   [path: string]: Array<any>;
 };
 
@@ -12,17 +15,38 @@ export type Router = Node<In, Out>;
 
 const slice = Array.prototype.slice;
 
-export function createRouter<R extends Array<RegExp>>(routes: R): Router {
-  const paths = routes.map((route) => `r_${route}`);
-  return createNode<In, Out>(paths, (outputs) => {
+export function createRouter<R extends Array<RegExp>>(templates: R): Router {
+  const fields = templates.map((template) => `r_${template}`);
+  return createNode<In, Out>
+  (["d_template", ...fields], (outputs) => {
+    const templateCount = templates.length;
+    let lastRoute: string;
+    let lastTemplate: RegExp;
+    let lastParams: Array<string>;
     return {
-      d_path: (value, tag) => {
-        for (const [idx, route] of routes.entries()) {
-          const hits = route.exec(value);
-          if (hits) {
-            outputs[paths[idx]](slice.call(hits, 1), tag);
-            break;
+      d_route: (value, tag) => {
+        if (value !== lastRoute) {
+          // looking for matching template
+          for (let i = 0; i < templateCount; i++) {
+            const template = templates[i];
+            const components = template.exec(value);
+            if (components) {
+              // template matches
+              if (template !== lastTemplate) {
+                // matching template changed
+                lastTemplate = template;
+                outputs.d_template(template, tag);
+              }
+              const params = slice.call(components, 1);
+              if (!eqArray(params, lastParams)) {
+                // parameters differ
+                outputs[fields[i]](params, tag);
+                lastParams = params;
+              }
+              break;
+            }
           }
+          lastRoute = value;
         }
       }
     };
