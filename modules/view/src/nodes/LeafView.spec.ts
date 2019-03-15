3581843545c9d7f0e58b3fefa2e6f2034beafe89
Dan Stocker <dan@kwaia.com>
2019-03-15 12:37:32 +0000

    Adds port 'ev_smp' to LeafView

--- modules/view/src/nodes/LeafView.spec.ts
+++ modules/view/src/nodes/LeafView.spec.ts
@@ -2,7 +2,37 @@ import {connect} from "river-core";
 import {createLeafView, LeafView} from "./LeafView";
 
 describe("createLeafView()", () => {
-  describe("on input (v_diff)", () => {
+  describe("on input (ev_smp)", () => {
+    let node: LeafView;
+
+    beforeEach(() => {
+      node = createLeafView(() => "style,color", 2);
+      node.i.vm_diff({
+        del: {},
+        set: {
+          "page.table.1-3.text": "Bar",
+          "page.table.2-4.text": "Foo"
+        }
+      }, "1");
+    });
+
+    it("should emit matching contents on 'v_diff'", () => {
+      const spy = jasmine.createSpy();
+      connect(node.o.v_diff, spy);
+      node.i.ev_smp({
+        "page.table.1-3.text": null,
+        "page.table.3-2.text": null
+      }, "1");
+      expect(spy).toHaveBeenCalledWith({
+        del: {},
+        set: {
+          "page.table.1-3.text": "Bar"
+        }
+      }, "1");
+    });
+  });
+
+  describe("on input (vm_diff)", () => {
     let node: LeafView;
 
     beforeEach(() => {
--- modules/view/src/nodes/LeafView.ts
+++ modules/view/src/nodes/LeafView.ts
@@ -1,9 +1,10 @@
-import {Diff, replacePathTail2} from "gravel-core";
+import {applyDiff, Diff, filterLookup, replacePathTail2} from "gravel-core";
 import {Any, createNode, Node} from "river-core";
 
 export type PathMapperCallback = (path: string) => string;
 
 export type In = {
+  ev_smp: Any;
   vm_diff: Diff<Any>;
 };
 
@@ -13,25 +14,38 @@ export type Out = {
 
 export type LeafView = Node<In, Out>;
 
-// TODO: Sampling?
 // TODO: Is a callback necessary? Are there any other use cases than string?
 //  (Perhaps when the property to be written is not known beforehand.)
 export function createLeafView(
   cb: PathMapperCallback,
   depth: number = 0
 ): LeafView {
+  const cache: Any = {};
   return createNode<In, Out>(["v_diff"], (outputs) => ({
+    ev_smp: (value, tag) => {
+      const filtered = filterLookup(cache, value);
+      if (filtered) {
+        outputs.v_diff({
+          del: {},
+          set: filtered
+        }, tag);
+      }
+    },
+
     vm_diff: (value, tag) => {
-      const result = {set: {}, del: {}};
-      for (const abs in value.set) {
-        const abs2 = replacePathTail2(abs, depth, cb);
-        result.set[abs2] = value.set[abs];
+      const changed = applyDiff(value, cache);
+      if (changed) {
+        const result = {set: {}, del: {}};
+        for (const abs in value.set) {
+          const abs2 = replacePathTail2(abs, depth, cb);
+          result.set[abs2] = value.set[abs];
+        }
+        for (const abs in value.del) {
+          const abs2 = replacePathTail2(abs, depth, cb);
+          result.del[abs2] = null;
+        }
+        outputs.v_diff(result, tag);
       }
-      for (const abs in value.del) {
-        const abs2 = replacePathTail2(abs, depth, cb);
-        result.del[abs2] = null;
-      }
-      outputs.v_diff(result, tag);
     }
   }));
 }
