112b5c3be2c6bbae48e56a4e3d97054616ee1dcd
Dan Stocker <dan@kwaia.com>
2019-03-16 21:39:16 +0000

    Adds DomLinkView2

---
+++ modules/view-dom-lib/src/nodes/DomLinkView2.spec.ts
@@ -0,0 +1,66 @@
+import {connect} from "river-core";
+import {createDomLinkView2, DomLinkView2} from "./DomLinkView2";
+
+describe("createDomLinkView2()", () => {
+  describe("on input (d_vm)", () => {
+    let node: DomLinkView2;
+
+    beforeEach(() => {
+      node = createDomLinkView2(() => "childNodes.0:a", 0);
+    });
+
+    describe("on set", () => {
+      it("should emit mapped vm on 'd_view'", () => {
+        const spy = jasmine.createSpy();
+        connect(node.o.d_view, spy);
+        node.i.d_vm({
+          del: {},
+          set: {
+            "foo.content": "Hello",
+            "foo.url": "http://"
+          }
+        }, "1");
+        expect(spy).toHaveBeenCalledWith({
+          del: {},
+          set: {"childNodes.0:a.innerText": "Hello"}
+        }, "1");
+        expect(spy).toHaveBeenCalledWith({
+          del: {},
+          set: {"childNodes.0:a.href": "http://"}
+        }, "1");
+      });
+    });
+
+    describe("on del", () => {
+      beforeEach(() => {
+        node.i.d_vm({
+          del: {},
+          set: {
+            "foo.content": "Hello",
+            "foo.url": "http://"
+          }
+        }, "1");
+      });
+
+      it("should emit mapped vm on 'd_view'", () => {
+        const spy = jasmine.createSpy();
+        connect(node.o.d_view, spy);
+        node.i.d_vm({
+          del: {
+            "foo.content": null,
+            "foo.url": null
+          },
+          set: {}
+        }, "2");
+        expect(spy).toHaveBeenCalledWith({
+          del: {"childNodes.0:a.innerText": null},
+          set: {}
+        }, "2");
+        expect(spy).toHaveBeenCalledWith({
+          del: {"childNodes.0:a.href": null},
+          set: {}
+        }, "2");
+      });
+    });
+  });
+});
---
+++ modules/view-dom-lib/src/nodes/DomLinkView2.ts
@@ -0,0 +1,36 @@
+import {createDiffSplitter} from "gravel-core";
+import {
+  createParentView,
+  ParentViewIn,
+  ParentViewOut,
+  PathMapperCallback
+} from "gravel-view";
+import {createDomPropertyView2} from "gravel-view-dom";
+import {connect, Node} from "river-core";
+
+export type In = ParentViewIn;
+
+export type Out = ParentViewOut;
+
+export type DomLinkView2 = Node<In, Out>;
+
+export function createDomLinkView2(
+  cb: PathMapperCallback,
+  depth: number = 0
+): DomLinkView2 {
+  const textView = createDomPropertyView2("innerText");
+  const urlView = createDomPropertyView2("href");
+  const view = createParentView(cb, depth);
+  const splitter = createDiffSplitter<"d_text" | "d_url">({
+    d_text: ["content"],
+    d_url: ["url"]
+  }, depth + 1);
+
+  connect(view.o.d_vm, splitter.i.d_diff);
+  connect(splitter.o.d_text, textView.i.d_vm);
+  connect(splitter.o.d_url, urlView.i.d_vm);
+  connect(textView.o.d_view, view.i.d_view);
+  connect(urlView.o.d_view, view.i.d_view);
+
+  return view;
+}
