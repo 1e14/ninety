3b002193c2eb70ec5dba4b59911d96190ec88243
Dan Stocker <dan@kwaia.com>
2019-03-17 16:53:01 +0000

    Removes DomTextView

--- modules/view-dom-lib/src/nodes/index.ts
+++ modules/view-dom-lib/src/nodes/index.ts
@@ -1,4 +1,3 @@
 export {DomEventView, createDomEventView} from "./DomEventView";
 export {DomLinkView, createDomLinkView} from "./DomLinkView";
-export {DomTextView, createDomTextView} from "./DomTextView";
 export {DomTextView2, createDomTextView2} from "./DomTextView2";
