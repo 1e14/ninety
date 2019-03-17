345b8aacc0cea6a5764e9dff87225699c8f9477a
Dan Stocker <dan@kwaia.com>
2019-03-17 17:05:38 +0000

    Removes SimpleTableView from demo

--- modules/demo/src/nodes/index.ts
+++ modules/demo/src/nodes/index.ts
@@ -2,6 +2,5 @@ export {
   HelloWorldPageView, createHelloWorldPageView
 } from "./HelloWorldPageView";
 export {MainMenuView, createMainMenuView} from "./MainMenuView";
-export {SimpleTableView, createSimpleTableView} from "./SimpleTableView";
 export {Ticker, createTicker} from "./Ticker";
 export {MainPageView, createMainPageView} from "./MainPageView";
