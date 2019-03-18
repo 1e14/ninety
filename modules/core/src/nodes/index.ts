d9108a34d14aa226f1539627d96fb272e43678f6
Dan Stocker <dan@kwaia.com>
2019-03-18 00:46:11 +0000

    Removes DiffPrefixer

--- modules/core/src/nodes/index.ts
+++ modules/core/src/nodes/index.ts
@@ -1,3 +1,2 @@
 export {DiffBuffer, createDiffBuffer} from "./DiffBuffer";
-export {DiffPrefixer, createDiffPrefixer} from "./DiffPrefixer";
 export {FlameSplitter, createFlameSplitter} from "./FlameSplitter";
