6fc26757ab3859095df6f70dd37c7ed03fe67bc0
Dan Stocker <dan@kwaia.com>
2019-03-21 22:57:05 +0000

    Adds DOM-related nodes from old river-browser

--- modules/view-dom/src/nodes/index.ts
+++ modules/view-dom/src/nodes/index.ts
@@ -2,4 +2,6 @@ export {DomAttributeView, createDomAttributeView} from "./DomAttributeView";
 export {DomClassView, createDomClassView} from "./DomClassView";
 export {DomDiffApplier, createDomDiffApplier} from "./DomDiffApplier";
 export {DomPropertyView, createDomPropertyView} from "./DomPropertyView";
+export {DomReadyNotifier, createDomReadyNotifier} from "./DomReadyNotifier";
 export {DomStyleView, createDomStyleView} from "./DomStyleView";
+export {LocationHash, createLocationHash} from "./LocationHash";
