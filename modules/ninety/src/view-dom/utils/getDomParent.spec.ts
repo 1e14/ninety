d17e14b9316e3a3ffd26e9e159b2959ed68d8271
Dan Stocker <dan@kwaia.com>
2019-03-30 04:32:57 +0000

    Fixes cache building in getDomParent()

--- modules/ninety/src/view-dom/utils/getDomParent.spec.ts
+++ modules/ninety/src/view-dom/utils/getDomParent.spec.ts
@@ -14,10 +14,65 @@ describe("getDomParent()", () => {
     });
   });
 
+  it("should build cache", () => {
+    const tree: any = {};
+    const cache: any = {"": tree};
+    getDomParent(cache, "foo.bar.baz.quux");
+    expect(cache).toEqual({
+      "": {
+        foo: {
+          bar: {
+            baz: {}
+          }
+        }
+      },
+      "foo.": {
+        bar: {
+          baz: {}
+        }
+      },
+      "foo.bar.": {
+        baz: {}
+      },
+      "foo.bar.baz.": {}
+    });
+  });
+
   it("should return parent node", () => {
     const tree: any = {};
     const cache = {"": tree};
     expect(getDomParent(cache, "foo.bar.baz.quux"))
     .toBe(tree.foo.bar.baz);
+  });
+
+  describe("when tree already complete", () => {
+    it("should build cache", () => {//
+      const tree: any = {
+        foo: {
+          bar: {
+            baz: {}
+          }
+        }};
+      const cache: any = {"": tree};
+      getDomParent(cache, "foo.bar.baz.quux");
+      expect(cache).toEqual({
+        "": {
+          foo: {
+            bar: {
+              baz: {}
+            }
+          }
+        },
+        "foo.": {
+          bar: {
+            baz: {}
+          }
+        },
+        "foo.bar.": {
+          baz: {}
+        },
+        "foo.bar.baz.": {}
+      });
+    });
   });
 });
--- modules/ninety/src/view-dom/utils/getDomParent.ts
+++ modules/ninety/src/view-dom/utils/getDomParent.ts
@@ -2,7 +2,7 @@ import {Flame} from "../../flame/types";
 import {PATH_DELIMITER} from "../../flame/utils";
 
 export function getDomParent(cache: Flame, path: string, from: number = 0): any {
-  let parent: any;
+  let next: any;
   for (
     let to = path.indexOf(PATH_DELIMITER, from);
     to > -1;
@@ -10,12 +10,17 @@ export function getDomParent(cache: Flame, path: string, from: number = 0): any 
   ) {
     const root = path.slice(0, from);
     const component = path.slice(from, to);
-    const node = cache[root];
-    parent = node[component]; // TODO
-    if (!parent) {
-      parent = node[component] = {}; // TODO
-      cache[root + component + PATH_DELIMITER] = parent;
+    const property = cache[root];
+
+    next = property[component]; // TODO
+    if (!next) {
+      next = property[component] = {}; // TODO
+    }
+
+    const parentPath = root + component + PATH_DELIMITER;
+    if (!cache[parentPath]) {
+      cache[parentPath] = next;
     }
   }
-  return parent;
+  return next;
 }
