9e8c01709a198f770c7e064fe0179bd8acda0f81
Dan Stocker <dan@kwaia.com>
2019-03-31 10:42:58 +0100

    Fixes getForkPos()

--- modules/ninety/src/flame/utils/getForkPos.spec.ts
+++ modules/ninety/src/flame/utils/getForkPos.spec.ts
@@ -4,5 +4,7 @@ describe("getForkPos()", () => {
   it("should return position of fork", () => {
     expect(getForkPos("foo.bar.baz", "foo.bar.quux"))
     .toBe(8);
+    expect(getForkPos("foo.bar.baz", "foo.bar.baa"))
+    .toBe(8);
   });
 });
--- modules/ninety/src/flame/utils/getForkPos.ts
+++ modules/ninety/src/flame/utils/getForkPos.ts
@@ -1,7 +1,11 @@
+import {PATH_DELIMITER} from "./path";
+
 export function getForkPos(path1: string, path2: string): number {
-  let i = 0;
-  while (path1[i] === path2[i]) {
-    i++;
+  let pos = 0;
+  for (let i = 0; path1[i] === path2[i]; i++) {
+    if (path1[i] === PATH_DELIMITER) {
+      pos = i + 1;
+    }
   }
-  return i;
+  return pos;
 }
