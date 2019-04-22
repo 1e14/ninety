22fa11629d81c803e9376162c37f25a9aeee735b
danstocker <dan@kwaia.com>
2019-04-22 09:33:38 +0100

    Adds documentation to MainModel.

--- modules/ninety-model/src/nodes/MainModel.ts
+++ modules/ninety-model/src/nodes/MainModel.ts
@@ -2,15 +2,23 @@ import {createNoop, Node} from "1e14";
 import {Flame} from "flamejet";
 
 export type In = {
+  /** Model received from expanders. */
   d_model: Flame;
 };
 
 export type Out = {
+  /** Model passed on to view-models. */
   d_model: Flame;
 };
 
+/**
+ * Joins all (expanded) models tobe processed by the view-model layer.
+ */
 export type MainModel = Node<In, Out>;
 
+/**
+ * Creates a MainModule node.
+ */
 export function createMainModel(): MainModel {
   const model = createNoop();
   return {
