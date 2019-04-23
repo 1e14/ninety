34db3f4128a22c7e25de79e169610f2b93bc5ef9
Dan Stocker <dan@kwaia.com>
2019-04-23 12:54:14 +0100

    Adds documentation to ReferenceFieldTypes

--- modules/ninety-model/src/types/ReferenceFieldTypes.ts
+++ modules/ninety-model/src/types/ReferenceFieldTypes.ts
@@ -1,1 +1,10 @@
-export type ReferenceFieldTypes = { [field: string]: string };
+/**
+ * Associates model types to reference fields of a model.
+ * @example
+ * { author: "Author", publisher: "Publisher" }
+ * // lowercase: field
+ * // capitalized: model type
+ */
+export type ReferenceFieldTypes = {
+  [field: string]: string
+};
